e311b972a8753af17d7f4b2897f996bc81c3f748
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-11-08 00:15:00 +0100

    initialize project structure and configuration

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}></Route>
+    </Routes>
+  )
+}
+
+export default App
