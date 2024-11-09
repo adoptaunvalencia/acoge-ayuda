973a3e274bba9fe1471a4d6e65a68e2b11fdfb7a
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-11-09 12:11:21 +0100

    FIX Button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import './button.css';
-import SearchIcon from '../../assets/icons/search-icon.svg';
-import EditIcon from '../../assets/icons/edit-icon.svg';
-import WhatsappIcon from '../../assets/icons/whatsapp-icon.svg';
+import React from 'react'
+import './button.css'
 
-const Button = ({ text, icon, width, bgColor, action }) => {
-
-  const iconSrc = icon === 'search' ? SearchIcon :
-                  icon === 'edit' ? EditIcon :
-                  icon === 'whatsapp' ? WhatsappIcon :
-                  null;
-
+const Button = ({
+  text,
+  icon,
+  bgColor,
+  textColor,
+  action = () => {},
+  disabled = false
+}) => {
   return (
     <button
-      className={`button ${bgColor === 'white' ? 'button-white' : 'button-red'}`}
-      style={{ width }}
+      className='button-component'
       onClick={action}
+      disabled={disabled}
+      style={{
+        backgroundColor: bgColor ? bgColor : 'var(--bg-lighter-gray)',
+        color: textColor ? textColor : 'var(--text-primary'
+      }}
     >
-      {iconSrc && <img src={iconSrc} className="button-icon" />}
-      {text}
+      {icon && <span className='icon'>{icon}</span>}
+      {text && { text }}
     </button>
   )
 }
 
-export default Button;
+export default Button
