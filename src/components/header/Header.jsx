7536ff216e60fc7452fda5ce2492bdd663084050
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-11-09 16:40:22 +0100

    UPDATE

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,33 @@
 import Button from '../button/Button'
-import logo from '../../assets/icons/logo.svg'
+import logo from '../../assets/icons/Logo.svg'
 import './Header.css'
+
+/**
+ * 
+ *  <Button
+  text='Get Offers'
+  icon={myIcon}
+  bgColor='#FF5733'
+  textColor='white'
+  action={getOffers}
+/>
+ */
+
+const handleCreateHelp = () => {
+  console.log('Hola');
+  
+}
 
 const Header = () => {
   return (
     <div>
       <div>
-        {logo}
+        <img src={logo} width='57' />
+        <h1>Adopta un <br></br> Valenciano</h1>
       </div>
-      <div>hola</div>
+      <div>
+        <Button text='Publicar ayuda' action={handleCreateHelp} />
+      </div>
     </div>
   )
 }
