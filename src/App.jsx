93f0f20376ffb563ea7f647cc157d511a2f6210d
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-11-09 13:03:00 +0100

    FEAT ProtectedRoute and implement in App.jsx, Dashboard, etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './layout/Layout'
+import ProtectedRoute from './components/protected-route/ProtectedRoute'
 import Home from './pages/Home/Home'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout />}></Route>
       <Route index element={<Home />}></Route>
+      <Route
+        path='dashboard'
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      ></Route>
     </Routes>
   )
 }
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,14 @@
+import { useContext } from 'react'
 import { Outlet } from 'react-router-dom'
+import { ReducerContext } from '../contexts/reducer.contexts/ReducerContext'
 
 const Layout = () => {
+  const {
+    stateIsAuth: { user, isAuth }
+  } = useContext(ReducerContext)
+  console.log(user)
+  console.log(isAuth)
+
   return <div>{Outlet}</div>
 }
 
