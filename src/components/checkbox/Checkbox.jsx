899e4c976a0f72b283e66b9b9d3346c51fcfc81c
Alberto Gonzalez <albergonmar@gmail.com>
2024-11-10 18:18:26 +0100

    add: Checkbox input created

---
+++ src/components/checkbox/Checkbox.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './checkbox.css'
+
+const Checkbox = (props) => {
+    const { label, name, id, required = false, className = "", onChange } = props
+
+    const handleChange = (event) => { if (onChange) onChange(event.target.checked) }
+
+    return (
+        <div className={`form-checkbox ${className}`}>
+            <input 
+                className={`checbox-input ${required && 'input-required'}`}
+                id={id} 
+                name={name} 
+                type="checkbox"
+                onChange={handleChange}
+                required 
+            />
+            { label && ( <label className="checkbox-label" htmlFor={id}>{label}</label> )}
+        </div>
+    )
+}
+
+export default Checkbox
