b7c36d6f1807621bcfb93e426c62b13ec2c97dca
PMFrancisco <francisco.perez.munoz@gmail.com>
2024-11-10 12:12:18 +0100

    feat: filter by geolocation

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import {
   MapContainer,
   Marker,
@@ -6,117 +6,165 @@ import {
   TileLayer,
   LayersControl,
   LayerGroup,
-  useMap
-} from 'react-leaflet'
-import L from 'leaflet'
-import useFetch from '../../hooks/useFetch'
-import 'leaflet/dist/leaflet.css'
-import accommodation from '../../assets/icons/accommodation.png'
-import hygiene from '../../assets/icons/hygiene.png'
-import food from '../../assets/icons/food.png'
-import pet_fostering from '../../assets/icons/pet_fostering.png'
-import def from '../../assets/icons/default.png'
+  useMap,
+} from "react-leaflet";
+import L from "leaflet";
+import useFetch from "../../hooks/useFetch";
+import "leaflet/dist/leaflet.css";
+import accommodation from "../../assets/icons/accommodation.png";
+import hygiene from "../../assets/icons/hygiene.png";
+import food from "../../assets/icons/food.png";
+import pet_fostering from "../../assets/icons/pet_fostering.png";
+import def from "../../assets/icons/default.png";
 
-const { BaseLayer, Overlay } = LayersControl
+const { BaseLayer, Overlay } = LayersControl;
+
 const offerIcons = {
   accommodation: new L.Icon({
     iconUrl: accommodation, // URL de tu icono
     iconSize: [32, 32], // Tamaño del icono
     iconAnchor: [16, 32], // Punto de anclaje del icono (la base del icono)
-    popupAnchor: [0, -32] // Posición del popup
+    popupAnchor: [0, -32], // Posición del popup
   }),
   hygiene: new L.Icon({
     iconUrl: hygiene,
     iconSize: [32, 32],
     iconAnchor: [16, 32],
-    popupAnchor: [0, -32]
+    popupAnchor: [0, -32],
   }),
   food: new L.Icon({
     iconUrl: food,
     iconSize: [32, 32],
     iconAnchor: [16, 32],
-    popupAnchor: [0, -32]
+    popupAnchor: [0, -32],
   }),
   pet_fostering: new L.Icon({
     iconUrl: pet_fostering,
     iconSize: [32, 32],
-    iconAnchor: [16, 32],
-    popupAnchor: [0, -32]
+    popupAnchor: [0, -32],
   }),
   default: new L.Icon({
     iconUrl: def,
     iconSize: [32, 32],
     iconAnchor: [16, 32],
-    popupAnchor: [0, -32]
-  })
+    popupAnchor: [0, -32],
+  }),
+};
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  const R = 6371;
+  const dLat = ((lat2 - lat1) * Math.PI) / 180;
+  const dLon = ((lon2 - lon1) * Math.PI) / 180;
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos((lat1 * Math.PI) / 180) *
+      Math.cos((lat2 * Math.PI) / 180) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c;
 }
 
-export const Map = () => {
-  const [activeType, setActiveType] = useState('all')
+export const Map = ({ maxDistance }) => {
+  const [userLocation, setUserLocation] = useState(null);
+  const [activeType, setActiveType] = useState("all");
   const [categorizedOffers, setCategorizedOffers] = useState({
     accommodation: [],
     hygiene: [],
     food: [],
     pet_fostering: [],
-    all: []
-  })
+    all: [],
+  });
+
   const {
     data: assistanceOffers,
-    stateLoad: { load }
+    stateLoad: { load },
   } = useFetch({
-    uri: 'https://developer-proyect-dana.vercel.app/secure/api/v1/assistance-offer',
-    method: 'GET'
-  })
-  
+    uri: "https://developer-proyect-dana.vercel.app/secure/api/v1/assistance-offer",
+    method: "GET",
+  });
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+        },
+        (error) =>
+          console.error("Error al obtener la ubicación:", error.message)
+      );
+    }
+  }, []);
+
   useEffect(() => {
     if (assistanceOffers?.assistancesOffers) {
+      let offersToFilter = assistanceOffers.assistancesOffers;
+
+      if (
+        userLocation &&
+        maxDistance !== null &&
+        maxDistance !== undefined &&
+        maxDistance > 0
+      ) {
+        offersToFilter = assistanceOffers.assistancesOffers.filter((offer) => {
+          const offerDistance = getDistanceFromLatLonInKm(
+            userLocation.latitude,
+            userLocation.longitude,
+            offer.location.coordinates[1],
+            offer.location.coordinates[0]
+          );
+          return offerDistance <= maxDistance;
+        });
+      }
+
       const newCategorizedOffers = {
         accommodation: [],
         hygiene: [],
         food: [],
         pet_fostering: [],
-        all: []
-      }
-      assistanceOffers.assistancesOffers.forEach((offer) => {
+        all: [],
+      };
+
+      offersToFilter.forEach((offer) => {
+        newCategorizedOffers.all.push(offer);
         offer.typeOffer.forEach((item) => {
-          newCategorizedOffers.all.push(offer)
           if (newCategorizedOffers[item.type]) {
-            newCategorizedOffers[item.type].push(offer)
+            newCategorizedOffers[item.type].push(offer);
           }
-        })
-      })
-      setCategorizedOffers(newCategorizedOffers)
+        });
+      });
+
+      setCategorizedOffers(newCategorizedOffers);
     }
-  }, [assistanceOffers])
-  const initialPosition = assistanceOffers?.assistancesOffers.length
-    ? [
-        assistanceOffers.assistancesOffers[0].location.coordinates[1],
-        assistanceOffers.assistancesOffers[0].location.coordinates[0]
-      ]
-    : [40.42372525496708, -3.678864358280353] // MADRID
+  }, [assistanceOffers, userLocation, maxDistance]);
+
+  const initialPosition = userLocation
+    ? [userLocation.latitude, userLocation.longitude]
+    : [40.42372525496708, -3.678864358280353]; // MADRID
+
   return (
     <>
       {load ? (
-        'Loading'
+        "Loading"
       ) : (
         <MapContainer
           center={initialPosition}
           zoom={5}
           scrollWheelZoom={false}
-          style={{ height: '35vh', width: '100%' }}
+          style={{ height: "35vh", width: "100%" }}
         >
           <LayersControl
-            position='topright'
+            position="topright"
             onChange={(e) => setActiveType(e.name)}
           >
-            <BaseLayer checked name='Ofertas'>
+            <BaseLayer checked name="Ofertas">
               <TileLayer
-                attribution='&copy; OpenStreetMap contributors'
-                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                attribution="&copy; OpenStreetMap contributors"
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
             </BaseLayer>
-
-            <Overlay checked={activeType === 'all'} name='Todas las ofertas'>
+            <Overlay checked={activeType === "all"} name="Todas las ofertas">
               <LayerGroup>
                 {categorizedOffers.all.map((offer, index) => (
                   <CustomMarker key={`${offer._id}-${index}`} offer={offer} />
@@ -124,7 +172,7 @@ export const Map = () => {
               </LayerGroup>
             </Overlay>
             {Object.keys(categorizedOffers).map((type) => {
-              if (type !== 'all') {
+              if (type !== "all") {
                 return (
                   <Overlay
                     key={type}
@@ -140,34 +188,35 @@ export const Map = () => {
                       ))}
                     </LayerGroup>
                   </Overlay>
-                )
+                );
               }
-              return null
+              return null;
             })}
           </LayersControl>
         </MapContainer>
       )}
     </>
-  )
-}
+  );
+};
+
 const CustomMarker = ({ offer }) => {
-  const map = useMap()
+  const map = useMap();
   const handleClickPosition = () => {
     map.setView(
       [offer.location.coordinates[1], offer.location.coordinates[0]],
-      13, // Nivel de zoom deseado
+      13,
       { animate: true, duration: 0.8 }
-    )
-    console.log(offer)
-  }
+    );
+  };
   const offerType =
-    offer.typeOffer.length > 0 ? offer.typeOffer[0].type : 'default'
-  const markerIcon = offerIcons[offerType] || offerIcons.default
+    offer.typeOffer.length > 0 ? offer.typeOffer[0].type : "default";
+  const markerIcon = offerIcons[offerType] || offerIcons.default;
+
   return (
     <Marker
       position={[offer.location.coordinates[1], offer.location.coordinates[0]]}
       icon={markerIcon}
       eventHandlers={{ click: handleClickPosition }}
     ></Marker>
-  )
-}
+  );
+};
