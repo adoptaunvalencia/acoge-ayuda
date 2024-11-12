96f1dce1bcd8822defb497c874476fbc395281a5
enriquepaez <enrique.paezpozo@gmail.com>
2024-11-12 21:40:21 +0100

    CreateOffer form

--- src/reducers/offers.reducer/offer.action.js
+++ src/reducers/offers.reducer/offer.action.js
@@ -18,3 +18,19 @@ export const fetchOffers = async (uriApi, dispatchLoad, token = null) => {
 export const fetchFiltersRadius = async () => {
   const response = await fetch()
 }
+
+export const createOffer = async (formData, dispatchLoad, token) => {
+  const uriApi = 'assistance-offer/create-assitances'
+  try {
+    dispatchLoad({ type: 'LOAD_TRUE' })
+    const { response, data } = await fetchAuth(uriApi, formData, 'POST', token)
+    if (response.status !== 201) {
+      console.log('error')
+    }
+    return data
+  } catch (error) {
+    console.log(error)
+  } finally {
+    dispatchLoad({ type: 'LOAD_FALSE' })
+  }
+}
