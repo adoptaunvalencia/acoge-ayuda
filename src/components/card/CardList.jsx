b30091319ce9511007a64213baa7d6c66f4b6896
PMFrancisco <francisco.perez.munoz@gmail.com>
2024-11-14 20:35:50 +0100

    fix: cardlist now renders actives

--- src/components/card/CardList.jsx
+++ src/components/card/CardList.jsx
@@ -1,34 +1,41 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { ReducerContext } from '../../contexts/reducer.contexts/ReducerContext'
-import { FunctionContext } from '../../contexts/function.contexts/FunctionContext'
-import Card from './Card'
-import Spinner from '../spinner/Spinner'
-import Modal from '../modal/Modal'
-import './card.css'
+import React, { useState, useEffect, useContext } from 'react';
+import { ReducerContext } from '../../contexts/reducer.contexts/ReducerContext';
+import { FunctionContext } from '../../contexts/function.contexts/FunctionContext';
+import Card from './Card';
+import Spinner from '../spinner/Spinner';
+import Modal from '../modal/Modal';
+import './card.css';
 
-const CardList = ({ offers, activeTypes }) => {
-  const { dispatchOffer } = useContext(ReducerContext)
-  const { userLocation, filterOffers, categorizedOffers } = useContext(FunctionContext)
-  const [isLoading, setIsLoading] = useState(false)
+const CardList = ({ activeTypes }) => {
+  const { dispatchOffer } = useContext(ReducerContext);
+  const { userLocation, filterOffers, categorizedOffers } = useContext(FunctionContext);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    filterOffers(null, userLocation.radius, activeTypes)
-  }, [userLocation, activeTypes, filterOffers])
+    filterOffers(null, userLocation.radius, activeTypes);
+  }, [userLocation, activeTypes, filterOffers]);
 
-  const closeModal = () => setIsLoading(false)
+  const closeModal = () => setIsLoading(false);
 
   return (
     <div className='card-list'>
-      {categorizedOffers.all.map((offer, index) => (
-        <Card key={`${offer._id}-${index}`} offer={offer} />
-      ))}
+      {activeTypes.map(
+        (type) =>
+          categorizedOffers[type] && (
+            <React.Fragment key={type}>
+              {categorizedOffers[type].map((offer, index) => (
+                <Card key={`${offer._id}-${index}`} offer={offer} />
+              ))}
+            </React.Fragment>
+          )
+      )}
       {isLoading && (
         <Modal isModalOpen={isLoading} handleCloseModal={closeModal}>
           <Spinner />
         </Modal>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default CardList
+export default CardList;
